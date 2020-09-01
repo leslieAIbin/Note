bee326c97cde2c654e54dc0f6e2b3890ced5da4f
dutlzn <2436013662@qq.com>
2020-09-01 08:52:39 +0800

    add shiro code

---
+++ shiro/mybatis_generator/src/com/sxt/mapper/PermissionMapper.java
@@ -0,0 +1,17 @@
+package com.sxt.mapper;
+
+import com.sxt.domain.Permission;
+
+public interface PermissionMapper {
+    int deleteByPrimaryKey(Integer perid);
+
+    int insert(Permission record);
+
+    int insertSelective(Permission record);
+
+    Permission selectByPrimaryKey(Integer perid);
+
+    int updateByPrimaryKeySelective(Permission record);
+
+    int updateByPrimaryKey(Permission record);
+}
