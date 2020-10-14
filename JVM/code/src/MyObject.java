b0b112388c84c8c71278cf7e5757bde728f168f5
dutlzn <2436013662@qq.com>
2020-10-14 21:14:28 +0800

    jvm

---
+++ JVM/code/src/MyObject.java
@@ -0,0 +1,41 @@
+public class MyObject {
+
+    public static void m1() {
+        m1();
+    }
+
+
+    public void change(int a ){
+        a = 10;
+    }
+    public static void main(String[] args) {
+        // jdk是自带的
+        Object object = new Object();
+        // 这个模板是哪个快递员拿来的
+        System.out.println(object.getClass().getClassLoader());
+//        System.out.println(object.getClass().getClassLoader().getParent());
+//        System.out.println(object.getClass().getClassLoader().getParent().getParent());
+
+        System.out.println("==============================");
+
+        // 自定义的
+        MyObject myObject = new MyObject();
+        System.out.println(myObject.getClass().getClassLoader());
+        System.out.println(myObject.getClass().getClassLoader().getParent());
+        System.out.println(myObject.getClass().getClassLoader().getParent().getParent());
+
+        //
+//        Thread t1 = new Thread();
+////        t1.start();
+//        t1.start();
+//        Throwable
+
+
+//        m1();
+        int a = 100;
+        myObject.change(a);
+        System.out.println(a);
+
+
+    }
+}
